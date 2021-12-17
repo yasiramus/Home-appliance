54db6e69998ea225b4b801f0b1b9fa5dddbe1525
yasiramus <yasirayusif@gmail.com>
2021-12-17 09:08:59 -0800

    'updated'

--- Jss/furniture.js
+++ Jss/furniture.js
@@ -8,3 +8,29 @@
             // the classlist gives the list of the classes that container have(return an array)
             inputbutton.focus( );
         })
+
+//  slideshows
+var slideIndex = 1;
+showSlides(slideIndex);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+//image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
